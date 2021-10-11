10b19651e3d47924af6a24e37c28832592fca569
Ubuntu <stwadministrator@aiml-server-1.e1autctz1nkuxg2qocrr2so3rc.cx.internal.cloudapp.net>
2021-10-11 19:22:25 +0000

    Adding changes from server edition

--- face-processing/imageprocess.py
+++ face-processing/imageprocess.py
@@ -10,28 +10,34 @@ import matplotlib.pyplot as plt
 from skimage import io
 
 parser = argparse.ArgumentParser()
-#parser.add_argument("-f", "--file", dest="input_image", required="TRUE", type=argparse.FileType('r'))
+parser.add_argument("-f", "--file", dest="input_image", required="TRUE", type=argparse.FileType('r'))
 #parser.add_argument("-p", "--predictor", dest="landmark_predictor", required="TRUE", type=argparse.FileType('r'))
 
-#p = parser.parse_args()
+p = parser.parse_args()
 
 print(dlib.__version__)
 print(imutils.__version__)
 
-predictor_path = "shape_predictor_68_face_landmarks.dat"
+predictor_path = "shape_predictor_5_face_landmarks.dat"
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
-img = dlib.load_rgb_image('inputimage.jpg')
+img = dlib.load_rgb_image(p.input_image)
 
 rect = detector(img)[0]
 sp = predictor(img, rect)
 landmarks = np.array([[p.x, p.y] for p in sp.parts()])
 
 print(landmarks)
-for x in landmarks:
-    print(x)
-    
-f = open( 'inputimage.txt', 'w' )
+#for x in landmarks:
+    #print(x)
+ 
+#f = open( 'inputimage.txt', 'w' )
 #f.write( landmarks )
-f.close()
+#f.close()
+
+#np.savetxt("inputimage.txt",x)
+mat = np.matrix(landmarks)
+with open('inputimage.txt','wb') as f:
+    for line in mat:
+        np.savetxt(f, line, fmt='%.2f')
