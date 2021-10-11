3350efdc5d400fea655e4d583380d0715af4c6a3
Brendon Randall <brendon@brendonrandall.com>
2021-10-11 14:29:48 -0400

    simplify to landmarks on 68

--- face-processing/imageprocess.py
+++ face-processing/imageprocess.py
@@ -18,24 +18,14 @@ parser = argparse.ArgumentParser()
 print(dlib.__version__)
 print(imutils.__version__)
 
-predictor_path = "shape_predictor_5_face_landmarks.dat"
+predictor_path = "shape_predictor_68_face_landmarks.dat"
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
+img = dlib.load_rgb_image('inputimage.jpg')
 
-img = io.imread("inputimage.jpg")
+rect = detector(img)[0]
+sp = predictor(img, rect)
+landmarks = np.array([[p.x, p.y] for p in sp.parts()])
 
-dets = detector(img)
-
-#output face landmark points inside retangle
-#shape is points datatype
-#http://dlib.net/python/#dlib.point
-for k, d in enumerate(dets):
-    shape = predictor(img, d)
-
-vec = np.empty([68, 2], dtype = int)
-for b in range(68):
-    vec[b][0] = shape.part(b).x
-    vec[b][1] = shape.part(b).y
-
-print(vec)
+print(landmarks)
